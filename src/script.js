513cc3a6abcec64d4326c7348776d1b7d5012224
sarahiseffy <cloudchii@gmail.com>
2024-05-01 21:15:02 +0400

    Added script and id for icon of weather description

--- src/script.js
+++ src/script.js
@@ -6,7 +6,8 @@ function searchCity(city) { //1st function for the city for API data retrieval
   axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSearchSubmit(event) { //function for the search bar to prevent blank search
+function handleSearchSubmit(event) {
+  //function for the search bar to prevent blank search
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-input"); //this will target the form that will the user will input.
@@ -19,7 +20,6 @@ searchFormElement.addEventListener("submit", handleSearchSubmit); //this will ac
 
 searchCity("Paris"); //to default to this city once refreshed
 
-
 function refreshWeather(response) {
   //2nd function for the temperature for API data retrieval
   let cityElement = document.querySelector("#city"); //to target the city variable make sure to add id
@@ -30,16 +30,20 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000); // new Date code to parse the time code
+  let iconElement = document.querySelector("#weather-app-icon");//this will target the icon element value
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon">`;//this will change the icon and pull image from API
   cityElement.innerHTML = `${response.data.city}`; // this will change the city element with what is being logged on the searchInput
   temperatureElement.innerHTML = Math.round(temperature); //this will change the temperature related to the city data the user has searched.
   descriptionElement.innerHTML = response.data.condition.description; //targets the description value element id
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`; //targets the humidity value element id
   windElement.innerHTML = `${response.data.wind.speed}km/h`; //targets the win value element id
-  timeElement.innerHTML = formatDate(date); //targets the time value element id
+  timeElement.innerHTML = formatDate(date); //this will change the time using function created to format date properly
+  console.log(response.data);
 }
 
-function formatDate(date) { //create this function to get better format of the time
+function formatDate(date) {
+  //create this function to get better format of the time
 
   let hours = date.getHours();
   let minutes = date.getMinutes();
@@ -54,11 +58,10 @@ function formatDate(date) { //create this function to get better format of the t
   ];
   let day = days[date.getDay()];
 
-if (minutes < 10) { //if the time is less than 10 hours, to add "0" before the minutes value (example 1:01 instead of 1:1)
-  minutes = `0${minutes}`;
-}
+  if (minutes < 10) {
+    //if the time is less than 10 hours, to add "0" before the minutes value (example 1:01 instead of 1:1)
+    minutes = `0${minutes}`;
+  }
 
   return `${day}, ${hours}:${minutes}`; //this will return the proper format output
-
 }
-
